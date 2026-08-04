6d91356f2876376a6a90f3b04fb51ff496a05833
gitonga-ik <gitonga.ik@gmail.com>
2026-08-04 13:33:44 +0300

    wip: switch local -> neon

--- frontend/prisma/prisma.ts
+++ frontend/prisma/prisma.ts
@@ -1,16 +1,10 @@
 import "dotenv/config";
-import { PrismaMariaDb } from "@prisma/adapter-mariadb";
-import { PrismaClient } from "@/app/generated/prisma/client";
+import { PrismaPg } from "@prisma/adapter-pg";
+import { PrismaClient } from "../generated/prisma/client";
 
-const adapter = new PrismaMariaDb({
-    host: process.env.DB_HOST || 'localhost',
-    port: 3306,
-    user: process.env.DB_USER,
-    password: process.env.DB_PASSWORD,
-    database: process.env.DB_NAME,
-    connectionLimit: 10
-})
+const connectionString = `${process.env.DATABASE_URL}`;
 
+const adapter = new PrismaPg({ connectionString });
 const prisma = new PrismaClient({ adapter });
 
 export { prisma };
